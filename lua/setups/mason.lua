e141e1a5d11c5c710390e0ef0ce0243a0507fd7c
jak0b <me@jak0b.com>
2024-09-30 20:37:11 +0200

    chore(mason): use empty Mason config

--- lua/setups/mason.lua
+++ lua/setups/mason.lua
@@ -1,9 +1,1 @@
-require("mason").setup({
-  ui = {
-    icons = {
-      package_installed = "✓",
-      package_pending = "➜",
-      package_uninstalled = "✗"
-    }
-  }
-})
+require("mason").setup {}
