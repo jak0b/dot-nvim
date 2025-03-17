9ca2a1c2b68598ebf6a5086199d9440943d07ea8
jak0b <me@jak0b.com>
2025-03-17 10:31:45 +0100

    chore(theme): enable transparency and terminal colors
    change colorscheme

--- lua/base.lua
+++ lua/base.lua
@@ -55,7 +55,12 @@ opt.smartindent = true -- autoindent new lines
 opt.background = 'dark'
 opt.termguicolors = true
 
-vim.cmd.colorscheme "github_dark_tritanopia"
+require('github-theme').setup({ options = {
+  transparent = true,
+  terminal_colors = true,
+}})
+
+vim.cmd.colorscheme "github_dark_high_contrast"
 
 -- don't auto commenting new lines
 vim.cmd [[au BufEnter * set fo-=c fo-=r fo-=o]]
