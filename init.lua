30579fceaa1473b613ef877388cfe0e335376475
jak0b <me@jak0b.com>
2025-07-03 13:50:46 +0200

    feat: move setups to init

--- init.lua
+++ init.lua
@@ -1,6 +1,21 @@
 -- vim: acd
 
-local pkgs = { 'packages', 'base', 'setups', 'mappings' }
+local pkgs = {
+  'packages',
+  'base',
+  'setups/telescope',
+  'setups/comments',
+  'setups/lsp',
+  'setups/git',
+  'setups/treesitter',
+  'setups/tree',
+  'setups/surround',
+  'setups/mason',
+  'setups/statusline',
+  'setups/cmp',
+  'setups/autopair',
+  'mappings'
+}
 
 for _, pkg in ipairs(pkgs) do
   require(pkg)
